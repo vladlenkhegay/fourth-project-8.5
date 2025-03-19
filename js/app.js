a65d83b48c607321f742f1226ced3a81507705fe
Vladlenkhegay <Irsinus95@gmail.com>
2025-03-19 11:21:30 +0900

    new

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const block = document.querySelector(".main__hover");
-    const text = block.querySelector(".text");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const block = document.querySelector(".main__hover");
+//     const text = block.querySelector(".text");
 
-    block.addEventListener("mouseenter", function () {
-        text.textContent = "Скрыть";
-    });
+//     block.addEventListener("mouseenter", function () {
+//         text.textContent = "Скрыть";
+//     });
 
-    block.addEventListener("mouseleave", function () {
-        text.textContent = "Наведи на меня";
-    });
-});
+//     block.addEventListener("mouseleave", function () {
+//         text.textContent = "Наведи на меня";
+//     });
+// });
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
@@ -18,4 +18,15 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
+});
+
+document.getElementById("toggleButton").addEventListener("click", function() {
+    let element = document.getElementById("hiddenElement");
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+        this.textContent = "Скрыть";
+    } else {
+        element.style.display = "none";
+        this.textContent = "Показать все";
+    }
 });
