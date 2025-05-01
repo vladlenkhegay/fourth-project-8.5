6e0cf6553adacd6e4d8d7e20978080a3c26876f7
Vladlenkhegay <Irsinus95@gmail.com>
2025-05-01 18:50:13 +0900

    save

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-
-
-
 document.getElementById("toggleButton").addEventListener("click", function () {
     let element = document.getElementById("hiddenElement");
     let buttonText = document.getElementById("toggleText");
@@ -10,11 +7,11 @@ document.getElementById("toggleButton").addEventListener("click", function () {
     if (element.style.display === "none") {
         element.style.display = "block";
         buttonText.textContent = "Скрыть";
-        buttonIcon.src = "image/dropup.png"; // Меняем картинку
+        buttonIcon.src = "image/dropup.png";
     } else {
         element.style.display = "none";
         buttonText.textContent = "Показать все";
-        buttonIcon.src = "image/icon.svg"; // Возвращаем картинку
+        buttonIcon.src = "image/icon.svg";
         buttonIcon.alt = "Показать";
     }
 });
@@ -28,51 +25,3 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
-
-// const openMenuLink = document.getElementById('openMenu');
-// const closeMenuLink = document.getElementById('closeMenu');
-// const menu1 = document.getElementById('menuaside');
-
-// openMenuLink.addEventListener('click', (event) => {
-//     event.preventDefault();  // Чтобы страница не прыгала вверх
-//     menu1.style.display = 'block';  // Показываем меню
-// });
-
-// closeMenuLink.addEventListener('click', (event) => {
-//     event.preventDefault();  
-//     menu1.style.display = 'none';  // Прячем меню
-// });
-
-// const openMenuLinkq = document.getElementById('openMenu-2');
-// const closeMenuLinkq = document.getElementById('closeMenu-2');
-// const menuq = document.getElementById('menu-2');
-
-// openMenuLinkq.addEventListener('click', (event) => {
-//     event.preventDefault();  // Чтобы страница не прыгала вверх
-//     menuq.style.display = 'block';  // Показываем меню
-// });
-
-// closeMenuLinkq.addEventListener('click', (event) => {
-//     event.preventDefault();  
-//     menuq.style.display = 'none';  // Прячем меню
-// });
-
-// function setupMenu(link1Id, link2Id, menuId) {
-//     let menu = document.getElementById(menuId);
-//     let isOpen = false; // Флаг состояния
-
-//     function toggleMenu(event) {
-//         event.preventDefault();
-//         isOpen = !isOpen; // Переключаем флаг
-//         menu.style.display = isOpen ? "block" : "none";
-//     }
-
-//     document.getElementById(link1Id).addEventListener("click", toggleMenu);
-//     document.getElementById(link2Id).addEventListener("click", toggleMenu);
-// }
-
-// // Настраиваем первое меню
-// setupMenu("link1", "link2", "menu1");
-
-// // Настраиваем второе меню
-// setupMenu("link3", "link4", "menu2");
